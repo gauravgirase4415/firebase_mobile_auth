4833a756c40d761ac07cdd04369b1882f38a502e
Gaurav Girase <gauravgirase4415@gmail.com>
2023-08-18 15:41:36 +0530

    login with phone number throgh the firabse

---
+++ android/app/src/main/kotlin/com/example/phone_auth_assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.phone_auth_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController otpController = TextEditingController();
+
+
+  String userNumber = '';
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+
+  var otpFieldVisibility = false;
+  var receivedID = '';
+
+  void verifyUserPhoneNumber() {
+     // auth.setSettings(appVerificationDisabledForTesting: true); // <-- here is the magic
+    auth.verifyPhoneNumber(
+      phoneNumber: userNumber,
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        await auth.signInWithCredential(credential).then(
+              (value) => print('Logged In Successfully'),
+        );
+      },
+      verificationFailed: (FirebaseAuthException e) {
+        print(e.message);
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        receivedID = verificationId;
+        otpFieldVisibility = true;
+        setState(() {});
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {},
+    );
+  }
+
+  Future<void> verifyOTPCode() async {
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(
+      verificationId: receivedID,
+      smsCode: otpController.text,
+    );
+    await auth
+        .signInWithCredential(credential)
+        .then((value) =>
+        Fluttertoast.showToast(
+        msg: 'OTP verified Successfully',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        fontSize: 20.0,
+        backgroundColor: Colors.green));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Phone Authentication',
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: IntlPhoneField(
+
+                controller: phoneController,
+                initialCountryCode: 'IN',
+                decoration: const InputDecoration(
+                  hintText: 'Phone Number',
+                  labelText: 'Phone',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (val) {
+                  userNumber = val.completeNumber;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Visibility(
+                visible: otpFieldVisibility,
+                child: TextField(
+                  controller: otpController,
+                  keyboardType: TextInputType.number
+                  ,
+                  decoration: const InputDecoration(
+                    hintText: 'OTP Code',
+                    labelText: 'OTP',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (otpFieldVisibility) {
+                  verifyOTPCode();
+                } else {
+                  verifyUserPhoneNumber();
+                }
+                FocusManager.instance.primaryFocus?.unfocus();
+              },
+              child: Text(
+                otpFieldVisibility ? 'Login' : 'Verify',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
